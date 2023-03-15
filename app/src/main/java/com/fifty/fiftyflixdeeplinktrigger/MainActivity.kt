214bd7da9620c5e717d99a85e28db847c56f6d84
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-15 19:04:50 +0530

    Created trigger for deeplinking movie detail page.

--- app/src/main/java/com/fifty/fiftyflixdeeplinktrigger/MainActivity.kt
+++ app/src/main/java/com/fifty/fiftyflixdeeplinktrigger/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.fifty.fiftyflixdeeplinktrigger
 
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.fifty.fiftyflixdeeplinktrigger.ui.theme.FiftyFlixDeepLinkTriggerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FiftyFlixDeepLinkTriggerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Button(onClick = {
+                        val intent = Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://abid-coding.com/505642")
+                        )
+                        val pendingIntent = TaskStackBuilder.create(applicationContext).run {
+                            addNextIntentWithParentStack(intent)
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                                )
+                            } else {
+                                getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT
+                                )
+                            }
+                        }
+                        pendingIntent.send()
+                    }) {
+                        Text(text = "Trigger deeplink")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FiftyFlixDeepLinkTriggerTheme {
-        Greeting("Android")
     }
 }
