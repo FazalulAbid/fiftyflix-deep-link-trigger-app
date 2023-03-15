e91993fb368809992f5f7c48fc2554cee22cf9d2
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-15 18:21:04 +0530

    Project created.

---
+++ app/src/main/java/com/fifty/fiftyflixdeeplinktrigger/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.fifty.fiftyflixdeeplinktrigger
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fifty.fiftyflixdeeplinktrigger.ui.theme.FiftyFlixDeepLinkTriggerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FiftyFlixDeepLinkTriggerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FiftyFlixDeepLinkTriggerTheme {
+        Greeting("Android")
+    }
+}
